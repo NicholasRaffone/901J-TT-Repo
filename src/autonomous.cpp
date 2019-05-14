ad3a2285a0e5ddc9a987f87b665cad73dd506eb2
20wildmanj <20wildmanj@asij.ac.jp>
2019-05-14 14:36:12 +0900

    fixed syntax
    i hate c++

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,5 +1,10 @@
 #include "main.h"
 #include "config.hpp"
+#include <math.h>
+#include <vector>
+
+
+
 //test
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -14,7 +19,7 @@
  */
 void autonomous() {}
 
-float[] track(){
+std::vector<float> track(){
   float wheelrad = 5;
 
   float lencval = 0;
@@ -43,43 +48,55 @@ float[] track(){
 
   float dtheta;
 
-  float[] d = new float[2];
-  float[] dtemp = new float[2];
-  float d1 = new float[2];
+
+  std::vector<float> d (2);
+  std::vector<float> dtemp (2);
+  std::vector <float> d1 (2);
 
   float rad;
   float angle;
+  float currentl;
+  float currentr;
+  float currentb;
+
   while(true){
+
+    currentl = LeftEncoder.get_value();
+    currentr = RightEncoder.get_value();
+    currentb = BackEncoder.get_value();
 
     dltheta = currentl - prevl;
     drtheta = currentr - prevr;
     dbtheta = currentb - prevb;
 
     //distance traveled by each wheel
-    dl = ((dltheta)/360) * 2 *pi * wheelrad;
-    dr = ((drtheta)/360) * 2 *pi * wheelrad;
-    ds = ((dbtheta)/360) * 2 *pi * wheelrad;
+    dl = ((dltheta)/360) * 2 *M_PI * wheelrad;
+    dr = ((drtheta)/360) * 2 *M_PI * wheelrad;
+    ds = ((dbtheta)/360) * 2 *M_PI * wheelrad;
 
     //update values
     prevl = currentl;
     prevr = currentr;
     prevb = currentb;
 
-    theta1 = thetar + 180*((dl-dr)/(rdis+ldis))/pi;//convert angle to degrees and add to initial angle to find new angle
+    theta1 = thetar + 180*((dl-dr)/(rdis+ldis))/M_PI;//convert angle to degrees and add to initial angle to find new angle
 
     dtheta = theta1 - theta0; //change in angle
 
     if(dtheta == 0.0){//if only vertical movement add vertical component dr
-      dtemp = [ds,dr];
+
+      dtemp.at(0) = ds;
+      dtemp.at(1) = dr;
     }else{
-      dtemp = 2 * sin(dtheta/2) * [ds/dtheta + bdis,dr/dtheta + rdis];
+      dtemp.at(0) = 2 * sin(dtheta/2) * (ds/dtheta + bdis);
+      dtemp.at(1) = 2 * sin(dtheta/2) * (dr/dtheta + rdis);
     }
 
     //calculate average orientation
     thetam = theta0 + dtheta/2;
 
     //convert position to polar
-    angle = arctan(dtemp[1] / dtemp[0]);
+    angle = atan(dtemp[1] / dtemp[0]);
     rad = sqrt(dtemp[0] * dtemp[0] + dtemp[1] * dtemp[1]);
 
     angle += -1 * thetam;
@@ -89,13 +106,14 @@ float[] track(){
     dtemp[1] = rad * sin(angle);
 
     //add position vector to old one
-    for(int i = 0; i < dtemp.length; i++){
+    for(int i = 0; i < 2; i++){
       d1[i] = d[i] + dtemp[i];
     }
 
       theta0 = theta1;
 
-      d = d1;
+      d[0] = d1[0];
+      d[1] = d1[1];
 
       //return new position
       return d1;
