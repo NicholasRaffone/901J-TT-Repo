7efd8d555c73bb1799594984a851780d852aff0a
Nicholas Raffone <nicholas.raffone@gmail.com>
2019-05-24 08:04:34 +0900

    a;ksdf
    asdf

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -4,7 +4,6 @@
 #include <vector>
 
 
-namespace WheelTracker{
   float wheelrad = 2.75;
 
   float lencval = 0;
@@ -16,7 +15,7 @@ namespace WheelTracker{
   float prevb = 0;
   float thetar = 90;//constant
   float theta1;
-  float theta0 = thetar;
+  float theta0 = 0;
   float thetam;
 
   float rdis = 4.72440945;
@@ -46,7 +45,8 @@ namespace WheelTracker{
 
 std::vector<float> track(){
 
-
+    //printf("Error: %f \n", theta1);
+    //printf("dtheta: %f \n", dtheta);
 
     currentl = LeftEncoder.get_value();
     currentr = RightEncoder.get_value();
@@ -69,18 +69,17 @@ std::vector<float> track(){
     theta1 = thetar + 180*((dl-dr)/(rdis+ldis))/M_PI;//convert angle to degrees and add to initial angle to find new angle
 
     dtheta = theta1 - theta0; //change in angle
+    //if(dtheta == 0.0){//if only vertical movement add vertical component dr
 
-    if(dtheta == 0.0){//if only vertical movement add vertical component dr
-
-      dtemp.at(0) = ds;
-      dtemp.at(1) = dr;
-    }else{
+    //  dtemp.at(0) = ds;
+  //    dtemp.at(1) = dr;
+    //}
       dtemp.at(0) = 2 * sin(dtheta/2) * (ds/dtheta + bdis);
       dtemp.at(1) = 2 * sin(dtheta/2) * (dr/dtheta + rdis);
 
 
     //calculate average orientation
-    thetam = theta0 + dtheta/2;
+    thetam = (theta0 + dtheta)/2;
 
     //convert position to polar
     angle = atan(dtemp[1] / dtemp[0]);
@@ -91,7 +90,7 @@ std::vector<float> track(){
     //convert back
     dtemp[0] = rad * cos(angle);
     dtemp[1] = rad * sin(angle);
-}
+//}
     //add position vector to old one
     for(int i = 0; i < 2; i++){
       d1[i] = d[i] + dtemp[i];
@@ -101,12 +100,14 @@ std::vector<float> track(){
 
       d[0] = d1[0];
       d[1] = d1[1];
-      d1[2] = theta0;
+      d1[2] = thetam;
       //return new position
+
+
       return d1;
     }
 
-}
+
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -153,7 +154,7 @@ void opcontrol() {
 				right_chain.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 			}
 
-			  RobotPosition = WheelTracker::track();
+			  RobotPosition = track();
 			  printf("X: %f\r\n",RobotPosition[0]);
 			  printf("Y: %f\r\n",RobotPosition[1]);
 			  printf("THETA: %f\r\n",RobotPosition[2]);
