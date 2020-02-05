37e6337986ad18de724350e1bd04d3c634f665d4
Nicholas Raffone <nicholas.raffone@gmail.com>
2020-02-05 09:10:40 +0900

    motionprofiling works

--- src/main.cpp
+++ src/main.cpp
@@ -132,7 +132,7 @@ auto bruh = std::make_shared<SkidSteerModel>(
 		50.0);
 
 		//max v, a, j- logger pro
-	okapi::PathfinderLimits limits({5.0,5.0,5.0});
+	okapi::PathfinderLimits limits({0.9,5.8,5.0});
 
 	okapi::ChassisScales scales({3.25_in, 13.85_in},imev5GreenTPR);
 
@@ -144,14 +144,27 @@ auto bruh = std::make_shared<SkidSteerModel>(
 	.buildMotionProfileController()
 	;
 
-	//profileController->generatePath({a},"patha");
+  /*profileController->moveTo(    {
+        {0_ft, 0_ft, 0_deg},  // Profile starting position, this will normally be (0, 0, 0)
+        {3.4_ft, 5_ft, 0_deg}
+      });*/
+
+left_wheel.move_velocity(200);
+right_wheel.move_velocity(200);
+left_chain.move_velocity(200);
+right_chain.move_velocity(200);
+pros::delay(2000);
+left_wheel.move_velocity(0);
+right_wheel.move_velocity(0);
+left_chain.move_velocity(0);
+right_chain.move_velocity(0);
 
 
 
 	// set the state to zero
 	//chassis->setState({0_in, 0_in, 0_deg});
 	//chassis->turnToAngle(90_deg);
-	while (true) {
+	/**while (true) {
 		double power = 200*master.get_analog(ANALOG_LEFT_Y)/127;
 		double turn = 200*master.get_analog(ANALOG_RIGHT_X)/127;
 		int left = (int)(pow(((power + turn)/200.0),2.0)*200.0);
@@ -189,7 +202,7 @@ auto bruh = std::make_shared<SkidSteerModel>(
       intake1.move_velocity(0);
       intake2.move_velocity(0);
     }
-  }
+  }**/
 	// turn 45 degrees and drive approximately 1.4 ft
 	//chassis->moveDistance(12_in);
 	//chassis->turnAngle(90_deg);
